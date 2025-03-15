9d5e395b969ad11a1e4a5b3c53ff732241a1f742
BlossomBuild <blossombuildLLC@outlook.com>
2025-03-14 22:48:50 -0700

    Added Task Listeners

--- ProjectBloom/Networking/DatabaseViewModel.swift
+++ ProjectBloom/Networking/DatabaseViewModel.swift
@@ -28,10 +28,14 @@ class DatabaseViewModel {
     }
     
     private(set) var userProjectsStatus: FetchStatus = .notStarted
+    private(set) var userActiveTasksStatus: FetchStatus = .notStarted
+    private(set) var userCompletedTasksStatus: FetchStatus = .notStarted
     
     private(set) var projectDeletedStatus: OperationStatus = .notStarted
     
     private var projectsListener: ListenerRegistration?
+    private var activeTasksListener: ListenerRegistration?
+    private var completedTasksListener: ListenerRegistration?
     
     var userProjects : [Project] = []
     var projectTasks : [ProjectTask] = []
@@ -60,11 +64,13 @@ class DatabaseViewModel {
             newProjectName: newProjectName
             
         )}
-            
+    
     
     func listenToUserProjects(user: User) {
+        guard projectsListener == nil else { return}
+        
         userProjectsStatus = .fetching
-        let listener = Firestore.firestore()
+        projectsListener = Firestore.firestore()
             .collection(FirebasePaths.projects.rawValue)
             .whereField(FirebasePaths.usersID.rawValue, arrayContains: user.uid)
             .addSnapshotListener { [weak self] snapshot, error in
@@ -92,13 +98,89 @@ class DatabaseViewModel {
                 self.userProjectsStatus = .success
                 print("User projects successfully fetched.")
             }
-        
-        projectsListener = listener
     }
     
     func stopListeningToUserProjects() {
         projectsListener?.remove()
         projectsListener = nil
         print("Stopped listening to user projects")
-     }
+    }
+    
+    
+    //MARK: Tasks Functions
+    func listenToProjectTasks(projectID: String, taskType: String) {
+        updateTaskStatus(for: taskType, status: .fetching)
+        let listener = Firestore.firestore()
+            .collection(FirebasePaths.projects.rawValue)
+            .document(projectID)
+            .collection(taskType)
+            .addSnapshotListener { [weak self] snapshot, error in
+                guard let self = self else {return}
+                
+                if let error = error {
+                    updateTaskStatus(for: taskType,
+                                     status: .failed(underlyingError: error))
+                    print("Error fetching project tasks: \(error.localizedDescription)")
+                    return
+                }
+                
+                guard let snapshot = snapshot else {
+                    updateTaskStatus(for: taskType, status: .failed(underlyingError: NSError(
+                        domain: "DatabaseViewModel",
+                        code: 404,
+                        userInfo: [NSLocalizedDescriptionKey: "No Tasks Found"]
+                    )))
+                    print("No Tasks Found")
+                    return
+                }
+                
+                do {
+                    try mapTasks(from: snapshot, to: taskType)
+                    updateTaskStatus(for: taskType, status: .success)
+                    print("Fetched Tasks: \(self.projectTasks)")
+                } catch {
+                    updateTaskStatus(for: taskType, status: .failed(underlyingError: error))
+                    print("Error decoding tasks: \(error.localizedDescription)")
+                }
+            }
+        
+        if taskType == FirebasePaths.projectTasks.rawValue {
+            activeTasksListener = listener
+        } else {
+            completedTasksListener = listener
+        }
+    }
+    
+    func stopListeningToProjectTasks(taskType: String) {
+        if taskType == FirebasePaths.projects.rawValue {
+            activeTasksListener?.remove()
+            activeTasksListener = nil
+            print("Stopped listening to active tasks")
+        } else {
+            completedTasksListener?.remove()
+            completedTasksListener = nil
+            print("Stopped listening to completed tasks")
+        }
+    }
+    
+    private func updateTaskStatus(for taskType: String, status: FetchStatus) {
+        if taskType == FirebasePaths.projectTasks.rawValue {
+            userActiveTasksStatus = status
+        } else {
+            userCompletedTasksStatus = status
+        }
+    }
+    
+    private func mapTasks(from snapshot: QuerySnapshot, to taskType:String) throws {
+        if taskType == FirebasePaths.projectTasks.rawValue {
+            self.projectTasks = try snapshot.documents.map { document in
+                return try document.data(as: ProjectTask.self)
+            }
+        } else {
+            self.completedTasks = try snapshot.documents.map { document in
+                return try document.data(as: ProjectTask.self)
+            }
+        }
+    }
+    
 }
