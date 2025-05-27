6a15cd3475ca507b4827901690b943aabb0ac2e0
BlossomBuild <blossombuildLLC@outlook.com>
2025-05-27 13:48:43 -0700

    Have most of the registartion logic check in place but not done yet | Also added progressView overlay

--- ProjectBloom/Features/Auth/Views/EmailPasswordView.swift
+++ ProjectBloom/Features/Auth/Views/EmailPasswordView.swift
@@ -11,6 +11,37 @@ struct EmailPasswordView: View {
     @State private var email = ""
     @State private var password = ""
     @State private var errorMessage : String?
+    @State private var isRegistering: Bool = false
+    
+    var isEmailValid: Bool {
+        let emailFormat =
+        "^[A-Z0-9a-z._%+-]+@(?:[A-Z0-9a-z-]+\\.)+[A-Za-z]{2,64}$"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailFormat)
+        return emailPredicate.evaluate(with: email)
+    }
+    
+    var isPasswordLongEnough: Bool {
+        password.count >= 8
+    }
+
+    var containsLetter: Bool {
+        password.range(of: "[A-Za-z]", options: .regularExpression) != nil
+    }
+
+    var containsNumber: Bool {
+        password.range(of: "\\d", options: .regularExpression) != nil
+    }
+
+    var containsSpecialCharacter: Bool {
+        password.range(of: "[!@#$%^&*()_+=\\-{}|:;\"'<>,.?/~`]", options: .regularExpression) != nil
+    }
+
+    var isPasswordStrong: Bool {
+        isPasswordLongEnough &&
+        containsLetter &&
+        containsNumber &&
+        containsSpecialCharacter
+    }
     
     var body: some View {
         VStack (spacing: 20) {
@@ -26,13 +57,39 @@ struct EmailPasswordView: View {
                 .background(.thinMaterial)
                 .clipShape(.rect(cornerRadius: 10))
             
-            Text(UIStrings.login.localizedKey)
+            
+            if(isRegistering) {
+                VStack(alignment: .leading) {
+                    Text("• At least 8 characters")
+                        .foregroundStyle(isPasswordLongEnough ? .bbWhite : .red)
+                    Text("• Includes a letter")
+                        .foregroundStyle(containsLetter ? .bbWhite : .red)
+                    Text("• Includes a number")
+                        .foregroundStyle(containsNumber ? .bbWhite : .red)
+                    Text("• Includes a special character")
+                        .foregroundStyle(containsSpecialCharacter ? .bbWhite : .red)
+                }
+            }
+            
+            Button {
                 
-        
+            } label: {
+                Text(!isRegistering ? UIStrings.login.localizedKey : UIStrings.register.localizedKey)
+                    .font(.headline)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .foregroundStyle(.bbWhite)
+                    .background(email.isEmpty || password.isEmpty || !isEmailValid ? .gray : .bbGreen)
+                    .clipShape(.rect(cornerRadius: 20))
+                    .disabled(email.isEmpty || password.isEmpty || isEmailValid)
+            }
+            
+            Button {
+                isRegistering.toggle()
+            } label: {
+                Text(isRegistering ? UIStrings.login.localizedKey : UIStrings.register.localizedKey)
+                    .foregroundStyle(.bbWhite)
+            }
         }
     }
-}
-
-#Preview {
-    EmailPasswordView()
 }
--- ProjectBloom/Features/Auth/Views/LoginView.swift
+++ ProjectBloom/Features/Auth/Views/LoginView.swift
@@ -11,78 +11,85 @@ import AuthenticationServices
 struct LoginView: View {
     @Environment(AuthViewModel.self) var authViewModel
     @Environment(\.dismiss) var dismiss
-    @State var showEmailLogin = false
     
     var body: some View {
         NavigationStack {
-                ScrollView {
-                    LazyVStack(spacing: 16) {
+            ScrollView {
+                LazyVStack(spacing: 16) {
+                    
+                    Text(UIStrings.appName.localizedKey)
+                        .foregroundStyle(Color(.bbWhite))
+                        .font(.poppinsFontBold)
+                        .padding(.top, 30)
+                        .padding(.bottom, 30)
+                    
+                    LoginButtonView(
+                        title: UIStrings.continueWithApple.localizedKey,
+                        iconName: Constants.appleLogo,
+                        isSystemImage: true
+                    ) {
                         
-                        Text(UIStrings.appName.localizedKey)
-                            .foregroundStyle(Color(.bbWhite))
-                            .font(.poppinsFontBold)
-                            .padding(.top, 100)
-                            .padding(.bottom, 100)
-                        
+                    }
+                    .disabled(authViewModel.isLoading)
+                    
+                    
+                    LoginButtonView(
+                        title: UIStrings.continueWithGoogle.localizedKey,
+                        iconName: Constants.google,
+                        isSystemImage: false
+                    ) {
+                        Task {
+                            await authViewModel
+                                .signInWithGoogle()
+                            dismiss()
+                        }
+                    }
+                    .disabled(authViewModel.isLoading)
+                    
+                    if authViewModel.authState == .signedOut {
                         LoginButtonView(
-                            title: UIStrings.continueWithApple.localizedKey,
-                            iconName: Constants.appleLogo,
+                            title: UIStrings.continueAnonymously.localizedKey,
+                            iconName: Constants.signedOutIcon,
                             isSystemImage: true
                         ) {
-                            
-                        }
-                        .disabled(authViewModel.isLoading)
-                        
-                        
-                        LoginButtonView(
-                            title: UIStrings.continueWithGoogle.localizedKey,
-                            iconName: Constants.google,
-                            isSystemImage: false
-                        ) {
                             Task {
-                                await authViewModel
-                                    .signInWithGoogle()
+                                await authViewModel.signInAnonymously()
                                 dismiss()
                             }
                         }
-                        .disabled(authViewModel.isLoading)
-                        
-                        
-                        LoginButtonView(
-                            title: UIStrings.continueWithEmail.localizedKey,
-                            iconName: Constants.emailIcon,
-                            isSystemImage: true
-                        ) {
-                            showEmailLogin.toggle()
-                        }
-                        .disabled(authViewModel.isLoading)
-                        
-                        if authViewModel.authState == .signedOut {
-                            LoginButtonView(
-                                title: UIStrings.continueAnonymously.localizedKey,
-                                iconName: Constants.signedOutIcon,
-                                isSystemImage: true
-                            ) {
-                                Task {
-                                    await authViewModel.signInAnonymously()
-                                    dismiss()
-                                }
-                            }
-                        }
-                        
-                        if showEmailLogin {
-                            Spacer()
-                            EmailPasswordView()
-                        }
-                        
-                        
-                        
                     }
+                    
+                    Spacer()
+                    
+                    Rectangle()
+                        .foregroundStyle(.bbWhite)
+                        .frame(height: 2)
+                    
+                    Spacer()
+                    
+                    EmailPasswordView()
+                    
+                    
+                    
+                    
                 }
+            }
             .padding(30)
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color(.bbGreenDark))
             .scrollIndicators(.hidden)
+            .overlay {
+                if authViewModel.isLoading {
+                    ZStack {
+                        Color.black.opacity(0.4).ignoresSafeArea() 
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle())
+                            .tint(.white)
+                            .scaleEffect(1.5)
+                    }
+                }
+                
+            }
         }
     }
 }
